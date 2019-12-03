edb19bd0d2ffc5ca2ec043d3767a5bb4ca212703
nina <ninamikulin@gmail.com>
2019-12-03 15:40:28 +0100

    Updated companies migration

--- database/migrations/2019_11_28_121307_create_companies_table.php
+++ database/migrations/2019_11_28_121307_create_companies_table.php
@@ -15,10 +15,17 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('name')->required();
             $table->string('email');
             $table->string('website');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
